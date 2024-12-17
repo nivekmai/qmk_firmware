14ced4008fbb08e739df261ecf4de81ec15918ad
Kevin Christensen <nivekmai@gmail.com>
2024-12-17 08:31:11 -0800

    Shift+Backspace == Delete

--- keyboards/zsa/moonlander/keymaps/nivekmai/keymap.c
+++ keyboards/zsa/moonlander/keymaps/nivekmai/keymap.c
@@ -74,6 +74,14 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   return true;
 }
 
+// Shift+Backspace == Delete
+const key_override_t delete_key_override = ko_make_basic(MOD_MASK_SHIFT, KC_BSPC, KC_DEL);
+
+// This globally defines all key overrides to be used
+const key_override_t **key_overrides = (const key_override_t *[]){
+	&delete_key_override,
+	NULL // Null terminate the array of overrides!
+};
 
 // ============================ END OVERRIDES ==================================
 
